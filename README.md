e6c7880079225a2d95cb4e0127ab9246f6fef644
AShaww <71328022+AShaww@users.noreply.github.com>
2022-05-17 20:32:49 +0100

    broke application/git using revert

---
+++ README.md
@@ -0,0 +1,4 @@
+# Change Log: Amheres.co.uk 
+
+Added About Section, CSS for titles etc. 
+
---
+++ assets/js/main.js
@@ -0,0 +1,91 @@
+(function() {
+    "use strict";
+    const select = (el, all = false) => {
+        el = el.trim()
+        if (all) {
+            return [...document.querySelectorAll(el)]
+        } else {
+            return document.querySelector(el)
+        }
+    }
+
+    const on = (type, el, listener, all = false) => {
+        let selectEl = select(el, all)
+        if (selectEl) {
+            if (all) {
+                selectEl.forEach(e => e.addEventListener(type, listener))
+            } else {
+                selectEl.addEventListener(type, listener)
+            }
+        }
+    }
+    const onscroll = (el, listener) => {
+        el.addEventListener('scroll', listener)
+    }
+
+    let navbarlinks = select('#navbar .scrollto', true)
+    const navbarlinksActive = () => {
+        let position = window.scrollY + 200
+        navbarlinks.forEach(navbarlink => {
+            if (!navbarlink.hash) return
+            let section = select(navbarlink.hash)
+            if (!section) return
+            if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+                navbarlink.classList.add('active')
+            } else {
+                navbarlink.classList.remove('active')
+            }
+        })
+    }
+    window.addEventListener('load', navbarlinksActive)
+    onscroll(document, navbarlinksActive)
+
+    const scrollto = (el) => {
+        let elementPos = select(el).offsetTop
+        window.scrollTo({
+            top: elementPos,
+            behavior: 'smooth'
+        })
+    }
+
+    on('click', '.mobile-nav-toggle', function(e) {
+        select('body').classList.toggle('mobile-nav-active')
+        this.classList.toggle('bi-list')
+        this.classList.toggle('bi-x')
+    })
+
+    const typed = select('.typed')
+    if (typed) {
+        let typed_strings = typed.getAttribute('data-typed-items')
+        typed_strings = typed_strings.split(',')
+        new Typed('.typed', {
+            strings: typed_strings,
+            loop: true,
+            typeSpeed: 100,
+            backSpeed: 50,
+            backDelay: 2000
+        });
+    }
+
+    window.addEventListener('load', () => {
+        AOS.init({
+            duration: 1000,
+            easing: 'ease-in-out',
+            once: true,
+            mirror: false
+        })
+    });
+
+})()
+var toggle = false;
+
+function toggleNav() {
+    var element = document.getElementById("body");
+    if (element.classList == "mobile-nav-active") {
+        (document.getElementById("main").style.marginLeft = "0");
+        (element.style.width = "0px");
+    } else {
+        (element.style.width = "250px");
+        (document.getElementById("main").style.marginLeft = "350px");
+    }
+}
